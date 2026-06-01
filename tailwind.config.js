6339a4bb4c854985af033d7100bf0aa61540b71c
Muhammad Moinudddin <moinuddin010203@gmail.com>
2026-06-01 08:19:09 +0500

    Contact Page 1st Section Completed

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./contact.html"], // 👈 Sirf contact.html rakhein taake baki pages mutasir na hon
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
