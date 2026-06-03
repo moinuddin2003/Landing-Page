7eac68c3cd2ca1745405e98e5d3dd5fe535652bc
moinuddin2003 <moinuddin010203@gmail.com>
2026-06-03 15:40:40 +0500

    Home Section Almost Completed

--- slider-init.js
+++ slider-init.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
   $('.testimonial-slider').slick({
     centerMode: true,
     centerPadding: '120px',
-    slidesToShow: 1,
+    slidesToShow: 2,
     infinite: true,
     dots: true,
     arrows: false,
@@ -52,4 +52,41 @@ $(document).ready(function() {
     owl.trigger('next.owl.carousel');
   });
 
+
+  $('.case-slider').slick({
+    centerMode: true,
+    centerPadding: '120px',
+    slidesToShow: 2,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    prevArrow: $('.prev-case'),
+    nextArrow: $('.next-case'),
+    responsive: [
+        { breakpoint: 1024, settings: { centerPadding: '60px' } },
+        { breakpoint: 768, settings: { centerMode: false, centerPadding: '0' } }
+    ]
 });
+
+
+$('.testimonial-s').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    prevArrow: $('.testi-prev'),
+    nextArrow: $('.testi-next'),
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: { slidesToShow: 1 }
+        }
+    ]
+});
+
+});
+
+//USed in index case studies slider
